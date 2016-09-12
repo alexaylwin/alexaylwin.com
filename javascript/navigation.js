f1711b45194b2c2b54020a448e4a4eb17f17fc01
Alex <AlexAylwin@gmail.com>
2016-09-12 16:08:47 -0400

    Added new section for skills Added new image for RBC Added new timeline items for RBC

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -4,22 +4,23 @@ $(document).ready(function(){
 	$('.navlink').removeClass('selected');
 	switch(linkId) {
       case 'homelink':
-        //$('.page').removeClass('page-active');
-        //$('#page-home').addClass('page-active');
         $("html, body").animate({ scrollTop: 0 }, "slow");
-		$(this).addClass("selected");
+		    $(this).addClass("selected");
         break;
       case 'projectslink':
         var pos = $("#page-projects").position();
         $("html, body").animate({ scrollTop: pos.top }, "slow");
-		$(this).addClass("selected");
-        //$('.page').removeClass('page-active');
-        //$('#page-projects').addClass('page-active');
+		    $(this).addClass("selected");
         break;
       case 'aboutmelink':
         var pos = $("#page-aboutme").position();
         $("html, body").animate({ scrollTop: pos.top }, "slow");
-		$(this).addClass("selected");
+        $(this).addClass("selected");
+        break;
+      case 'skillsetlink':
+        var pos = $("#page-skillset").position();
+        $("html, body").animate({ scrollTop: pos.top }, "slow");
+		    $(this).addClass("selected");
         break;		
     }
   })
